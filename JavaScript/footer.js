137517a9bca62975b7d9471967f3d452369b1286
ScratchOs <ScratchOs@users.noreply.github.com>
2015-10-01 17:18:13 +0100

    Update footer.js

--- JavaScript/footer.js
+++ JavaScript/footer.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
-  
+  if ($(window).height() > $("body").height()) {
+    $(".footer").css("position","absolute").css("bottom",0);
+  } else {
+    $(".footer").css("position","static")
+  }
 });
