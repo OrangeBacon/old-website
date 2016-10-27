b5bbb44ea05877de75a8f14c5557a7d286346632
ScratchOs <Computer.backup.15@gmail.com>
2016-10-27 09:00:49 +0100

    update

--- JavaScript/footer.js
+++
@@ -1,9 +0,0 @@
-function run() {
-  if ($(window).height() > $("body").height()) {
-    $(".footer").css("position","absolute").css("bottom",0);
-  } else {
-    $(".footer").css("position","static")
-  }
-};
-
-setInterval(run, 100);
--- README.md
+++
@@ -1,3 +0,0 @@
-# ScratchOs.github.io
-Welcome to my Website
-Includes scratchblocks plugin by tjvr.
